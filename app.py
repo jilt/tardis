5521e3fe67a497d9e65aafdf6a50bf91fefb3927
Agnese Camellini <joy@localhost.localdomain>
2019-06-05 22:50:28 +0200

    set up db mysql done

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:pippo@tardis.cuhorxnidu3b.us-west-2.rds.amazonaws.com/tardis_base'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    iduser = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(200), nullable=False)
+    token = db.Column(db.String(200), nullable=True)
+    date_ins = db.Column(db.DateTime, nullable=True)
+    date_ult_mod = db.Column(db.DateTime, nullable=True)
+    token_expire = db.Column(db.DateTime, nullable=True)
 
 @app.route('/')
 def index():
